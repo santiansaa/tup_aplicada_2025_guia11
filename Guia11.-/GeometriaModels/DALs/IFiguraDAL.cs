ec5cb20e9a33bb865c162fe9b0d27cc4648162c8
santiansaa <santiagoansa4@gmail.com>
2025-10-02 15:19:35 -0300

    dfdf

---
+++ Guia11.-/GeometriaModels/DALs/IFiguraDAL.cs
@@ -0,0 +1,16 @@
+﻿
+
+using GeometriaModels.Models;
+
+namespace GeometriaModels.DALs
+{
+    public interface IFiguraDAL
+    {
+        List<FiguraModel> GetAll();
+        FiguraModel GetById(int id);
+        FiguraModel Add(FiguraModel figura);
+        FiguraModel Save(FiguraModel entidad);
+        void Delete(int id);
+
+    }
+}
---
+++ Guia11.-/GeometriaModels/Models/FiguraModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometriaModels.Models
+{
+    internal class FiguraModel
+    {
+        public int? Id { get; set; }
+        public double? Area { get; set; }
+    }
+}
